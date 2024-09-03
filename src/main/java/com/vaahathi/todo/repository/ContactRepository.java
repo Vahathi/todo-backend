86d45a64bccc9ac68d53d8a47779963695aa9807
Nikitha-koukuntla <koukuntlanikhireddy@gmail.com>
2024-09-03 21:16:36 +0530

    fix: ordering for contact search api

--- src/main/java/com/vaahathi/todo/repository/ContactRepository.java
+++ src/main/java/com/vaahathi/todo/repository/ContactRepository.java
@@ -1,4 +1,5 @@
 package com.vaahathi.todo.repository;
+
 import com.vaahathi.todo.entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -6,11 +7,12 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
+
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, UUID> {
-    List<Contact> findByOwnerIdAndTaskTypeAndCategory(UUID ownerId, String taskType,String Category);
+    List<Contact> findByOwnerIdAndTaskTypeAndCategory(UUID ownerId, String taskType, String Category);
 
-    @Query("SELECT c FROM Contact c WHERE c.ownerId = :ownerId AND c.personName LIKE %:search%")
+    @Query("SELECT c FROM Contact c WHERE c.ownerId = :ownerId AND c.personName LIKE %:search% ORDER BY c.personName ASC")
     List<Contact> searchByOwnerIDAndName(UUID ownerId, String search);
 }
 
