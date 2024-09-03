628b4f49c0e14cb1ac801247025c03389cd0a78a
Nikitha-koukuntla <155812351+Nikitha-koukuntla@users.noreply.github.com>
2024-09-03 21:26:47 +0530

    feat: ordering contacts search (#12)

--- src/main/java/com/vaahathi/todo/repository/ContactRepository.java
+++ src/main/java/com/vaahathi/todo/repository/ContactRepository.java
@@ -6,11 +6,13 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
+
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, UUID> {
-    List<Contact> findByOwnerIdAndTaskTypeAndCategory(UUID ownerId, String taskType,String Category);
 
-    @Query("SELECT c FROM Contact c WHERE c.ownerId = :ownerId AND c.personName LIKE %:search%")
+    List<Contact> findByOwnerIdAndTaskTypeAndCategory(UUID ownerId, String taskType, String Category);
+
+    @Query("SELECT c FROM Contact c WHERE c.ownerId = :ownerId AND c.personName LIKE %:search% ORDER BY c.personName ASC")
     List<Contact> searchByOwnerIDAndName(UUID ownerId, String search);
 }
 
